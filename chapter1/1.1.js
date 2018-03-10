56c8c0d8dd819c82d8d4647e896ffff93a274e3b
Anton-Aleksei Ilyin <antonaleksei.ilyin@gmail.com>
2018-03-10 20:21:14 +0300

    Update 1.1.js

--- chapter1/1.1.js
+++ chapter1/1.1.js
@@ -1,1 +1,20 @@
+// Реализуйте алгоритм, определяющий, все ли символы в строке встречаются только один раз. 
+// А если при этом запрещено использование дополнительных структур данных?
 
+// My solution:
+function isUniqueChars(s = '') {
+    if (!s) {
+        return false;
+    }
+
+    let hash = {}, length = s.length;
+    for (var i = 0; i < length; i++) {
+        if (hash[s[i]]) {
+            return false;
+        }
+        hash[s[i]] = true;
+    }
+    return true;
+}
+
+// Almost the same in the book + additional solution using bitmap
